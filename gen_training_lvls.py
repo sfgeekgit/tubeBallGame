9276bf8626ab54ef148091bc4b6d4f66f7d63f84
sfgeekgit <sfgeek@gmail.com>
2023-11-29 18:02:05 -0800

    test data

--- gen_training_lvls.py
+++ gen_training_lvls.py
@@ -2,11 +2,10 @@ import level_gen
 
 
 
-
 level = level_gen.GameLevel()
 
 
-for i in range(500):
-    level.load_level_rand(4,6)
+for i in range(100):
+    level.load_level_rand(2,4)
     print("level" , level)
-    level.store_to_disk()
+    level.store_to_disk('../tubeballgame_stuff/easy_24_lvls/')
